27aca77bf600b8f047ae5545b37c264521269100
decodering <20130935+decodering@users.noreply.github.com>
2025-03-11 08:59:48 +0100

    added dummy_agent_library scripts

--- 1_agent_fundamentals/1_inference_example.py
+++ 1_agent_fundamentals/1_inference_example.py
@@ -1,5 +1,3 @@
-import os
-
 from dotenv import load_dotenv
 from huggingface_hub import InferenceClient
 
@@ -7,15 +5,30 @@ from huggingface_hub import InferenceClient
 # os.environ["HF_TOKEN"]="hf_xxxxxxxxxxxxxx"
 load_dotenv()
 
+STR_DIV = "\n=====================\n"
 
 client = InferenceClient("meta-llama/Llama-3.2-3B-Instruct")
 # if the outputs for next cells are wrong, the free model may be overloaded. You can also use this public endpoint that contains Llama-3.2-3B-Instruct
 # client = InferenceClient("https://jc26mwg228mkj8dw.us-east-1.aws.endpoints.huggingface.cloud")
 
-output = client.text_generation(
+example_prompts = [
     "The capital of France is",
-    max_new_tokens=100,
+    """<|begin_of_text|><|start_header_id|>user<|end_header_id|>
+The capital of France is<|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+]
+
+for ind, prompt in enumerate(example_prompts):
+    output = client.text_generation(
+        prompt,
+        max_new_tokens=100,
+    )
+    print(f"Prompt {ind} response{STR_DIV}{output.strip()}\n")
+
+output = client.chat.completions.create(
+    messages=[
+        {"role": "user", "content": "The capital of France is"},
+    ],
+    stream=False,
+    max_tokens=1024,
 )
-
-print(output)
-
+print(f"Prompt 3 response{STR_DIV}{output.choices[0].message.content}\n")
--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
+# HARDCODED
+VENV_BIN="venv/bin/activate"
+
 compile-requirements:
 	@pip-compile-multi --live
 
 sync-requirements:
-	@pip-sync requirements/*.txt
+	@source ${VENV_BIN} && pip-sync requirements/*.txt
 
 run:
 	@source venv/bin/activate && python main.py
